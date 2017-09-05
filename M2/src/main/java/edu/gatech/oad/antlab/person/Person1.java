7fa122ad4dd5dded08ab9135bc977187186cb5cc
agyscon 67 <agysconner67@gmail.com>
2017-09-05 14:20:23 -0400

    Modified Person.1. Still needs testing

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -30,8 +30,14 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+	  if (input.length() < 2) {
+	  	return input;
+	  } else {
+	    String firstTwo = input.substring(0, 2);
+	    String stringEnd = input.substring(2);
+
+	    return stringEnd + firstTwo;
+	  }
 	}
 	
 	/**
